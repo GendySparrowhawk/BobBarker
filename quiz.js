0caa5c9de98c5e59ae0be72d2d855d4ad3536a0f
Jacob Hoefer <jacob.hoefer@gmail.com>
2023-09-14 14:30:45 -0400

    haircut

--- quiz.js
+++ quiz.js
@@ -1,16 +1,19 @@
+// global variables//
 var startBtn = document.getElementById('start');
-
 var quizWindow = document.querySelector('.window');
 var timer = document.getElementById('timer');
 var timeLeft = 61;
-var btnWrapper = document.querySelectorAll('punch');
+var btnWrapper = document.querySelectorAll('.punch');
 
+// start quiz button//
 function startQuiz() {
     quizWindow.classList.remove('hide');
+    timer.classList.remove('hide');
     startBtn.classList.add('hide');
     countDown();
 }
 
+// start count down. stop count down//
 function countDown() {
     var stopTimer = setInterval(function () {
         timeLeft = timeLeft - 1
