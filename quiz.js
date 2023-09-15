acb85a5d995d05b6889fe5ad48dbd0d4b6abad23
Jacob Hoefer <jacob.hoefer@gmail.com>
2023-09-15 15:22:59 -0400

    blerg

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 // global variables//
-var startBtn = document.getElementById('start');
+var startBtn = document.querySelector('#start');
 var quizWindow = document.querySelector('.window');
-var timer = document.getElementById('timer');
+var timer = document.querySelector('#timer');
 var timeLeft = 61;
 var answers = document.querySelectorAll('#answers');
 var questionEl = document.querySelector("#question");
@@ -21,7 +21,7 @@ var quizQuestions = [
         ]
     },
     {
-        question: "What is the name of the spirit that Anthony William claims gave him the idea for celer juice?",
+        question: "What is the name of the spirit that Anthony William claims gave him the idea for celery juice?",
         answers: [
             { text: "Spirit", correct: true },
             { text: "Pazoozoo", correct: false },
@@ -31,14 +31,33 @@ var quizQuestions = [
 
     },
     {
-        question: "In which year did Gallagher's brother begin performing as Gallagher Too?",
+        question: "How many people have died in the Chicken-Sandwich Wars?",
         answers: [
-            { text: "1984", correct: false },
-            { text: "1986", correct: false },
-            { text: "1991", correct: true },
-            { text: "1965", correct: false },
+            { text: "4 deaths", correct: false },
+            { text: "1 death 12+ injured", correct: true },
+            { text: "5 deaths", correct: false },
+            { text: "0 deaths, 70+ injured", correct: false },
+        ]
+    },
+    {
+        question: "What was the 1910 so called Hippo Bill?",
+        answers: [
+            { text: "A law forbidding hippo poaching ", correct: false },
+            { text: "Outlawed all hippos from being transported across state lines", correct: false },
+            { text: "Authorized the import of hippos to eat invasive plants and slaughtered for meat", correct: true },
+            { text: "Subsidized farmers who used hippo \"fertilizer\"", correct: false },
+        ]
+    },
+    {
+        question: "What was the 1910 so called Hippo Bill?",
+        answers: [
+            { text: "A law forbidding hippo poaching ", correct: false },
+            { text: "Outlawed all hippos from being transported across state lines", correct: false },
+            { text: "Authorized the import of hippos to eat invasive plants and slaughtered for meat", correct: true },
+            { text: "Subsidized farmers who used hippo \"fertilizer\"", correct: false },
         ]
     }
+
 ]
 let currentQuestionIndex = 0;
 
@@ -73,13 +92,21 @@ function showQuestion() {
             button.dataset.correct = answer.correct
         }
         button.addEventListener('click', selectAnswer)
-        
+
     }
         
     });
 }
+makeAnwser(eventObj) {
+    if(eventObj.target) {
+        timeLeft -= -5
+
+    }
+
+}
 
 
+answers.addEventListener('click', makeAnswer() );
 // currentQuestion.answers.forEach(answer => {
 //     var button = document
 
